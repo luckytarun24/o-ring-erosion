9ef39b4bc7d8111c64bb9805ca552c29c3d950e3
luckytarun24 <51366078+luckytarun24@users.noreply.github.com>
2020-12-03 19:04:03 +0530

    Add files via upload

---
+++ code/rproject.cmd
@@ -0,0 +1,30 @@
+---
+title: "R Notebook"
+output: html_notebook
+---
+
+This is an [R Markdown](http://rmarkdown.rstudio.com) Notebook. When you execute code within the notebook, the results appear beneath the code. 
+
+Try executing this chunk by clicking the *Run* button within the chunk or by placing your cursor inside it and pressing *Ctrl+Shift+Enter*. 
+
+```{r}
+plot(cars)
+```
+
+Add a new chunk by clicking the *Insert Chunk* button on the toolbar or by pressing *Ctrl+Alt+I*.
+
+When you save the notebook, an HTML file containing the code and output will be saved alongside it (click the *Preview* button or press *Ctrl+Shift+K* to preview the HTML file).
+
+The preview shows you a rendered HTML copy of the contents of the editor. Consequently, unlike *Knit*, *Preview* does not run any R code chunks. Instead, the output of the chunk when it was last run in the editor is displayed.
+```{r}
+
+input_data <- read.csv("o-ring-erosion-only.csv")
+dim(input_data)
+str(input_data)
+summary(input_data)
+library(ggplot2)
+
+ggplot(input_data, aes(x= Launch_temperature, y = thermal_distress)) + geom_point(size = 2.5, color="navy") + xlab("Item Visibility") + ylab("Item Outlet Sales") + ggtitle("Item Visibility vs Item Outlet Sales")
+
+```
+]
